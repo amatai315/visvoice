9dd103c1cd3464380692eb0137cc1984d936c670
amatai315 <62067593+amatai315@users.noreply.github.com>
2020-11-28 14:06:14 +0900

    modify scraping_from_sakuhindb.py to get all data of one actor

--- scraping_from_sakuhindb.py
+++ scraping_from_sakuhindb.py
@@ -1,4 +1,5 @@
 import sys
+import time
 sys.path.append('/usr/local/lib/python3.9/site-packages')
 import requests
 from bs4 import BeautifulSoup
@@ -7,9 +8,16 @@ urlName = "https://sakuhindb.com/tj/6_BBD2B0C2C9F0BFCD/"
 url = requests.get(urlName)
 soup = BeautifulSoup(url.content, "html.parser")
 
-element_class_lk_th = soup.find(attrs={"class":"lk_th"}, text="ID")
-
-actor_data_list = element_class_lk_th.parent.next_siblings
-
-for actor_data in actor_data_list:
-    print(f"{actor_data.contents[2].contents[0]},{actor_data.contents[3].contents[0].contents[0]},{actor_data.contents[8].contents[0]}")
+while True:
+    actor_data_list = soup.find(attrs={"class":"lk_th"}, text="ID").parent.next_siblings
+    for actor_data in actor_data_list:
+        print(f"{actor_data.contents[2].contents[0]},{actor_data.contents[3].contents[0].contents[0]},{actor_data.contents[8].contents[0]}")
+    
+    next_link_tag = soup.find(attrs={"class":"lk_th"}, text="ID").parent.parent.next_sibling
+    if next_link_tag :
+        time.sleep(3)
+        next_link = "https://sakuhindb.com" + next_link_tag.get("href")
+        url = requests.get(next_link)
+        soup = BeautifulSoup(url.content, "html.parser")
+    else :
+        break
